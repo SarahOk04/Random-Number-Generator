172476116ec47ca783bc43bf01afd3c5fab6adbf
sarahok04 <sarahok04@gmail.com>
2020-07-16 01:45:16 -0600

    Initial commit

---
+++ Random Number Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Random_Number_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
